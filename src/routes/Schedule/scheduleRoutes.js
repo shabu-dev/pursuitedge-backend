f7aea5a717b8935219f79e46e582cf200e5a4e58
Shabhuddin <sayed.shabhuddin@mindscapecomputing.com>
2026-08-27 11:57:49 +0530

    schedule auth removed from the route

--- src/routes/Schedule/scheduleRoutes.js
+++ src/routes/Schedule/scheduleRoutes.js
@@ -5,9 +5,9 @@ const auth = require('../../middleware/auth');
 
 
 router.post("/create", auth, scheduleController.createSchedule);
-router.get("/get", auth, scheduleController.getSchedule);
+router.get("/get",  scheduleController.getSchedule);
 router.put("/update/:id", auth, scheduleController.updateSchedule);
 router.delete("/delete/:id", auth, scheduleController.deleteSchedule);
-router.post("/getSingleData",  auth, scheduleController.getScheduleSingleData);
+router.post("/getSingleData",  scheduleController.getScheduleSingleData);
 
 module.exports = router;
